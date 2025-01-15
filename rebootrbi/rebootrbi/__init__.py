001169cef49b924d6fba144193650a884a566c2e
grillribba <60137744+grillribbadev@users.noreply.github.com>
2025-01-15 17:09:33 +0100

    Delete rebootrbi directory

--- rebootrbi/rebootrbi/__init__.py
+++
@@ -1,9 +0,0 @@
-from .reboot import Reboot  # Import the cog class
-
-async def setup(bot):
-    """Setup function to add the cog to the bot."""
-    try:
-        await bot.add_cog(Reboot(bot))
-        print("Reboot cog loaded successfully.")
-    except Exception as e:
-        print(f"Error while loading Reboot cog: {e}")
